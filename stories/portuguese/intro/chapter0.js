0d915a2a67d0970f7e4d17aa04fb35fdcd4b3607
= <dupuqub@gmail.com>
2018-10-16 15:29:51 -0300

    images are cached again

--- stories/portuguese/intro/chapter0.js
+++ stories/portuguese/intro/chapter0.js
@@ -8,14 +8,14 @@ project.stories.portuguese.intro[0] = () =>
   const chapter =
   {
     title: `Bem vindo às Correntes, ${project.states.safe.avatar.name}.`,
-    image: `images/chooseBoat.png?${Date.now()}`,
+    image: `images/chooseBoat.png`,
     text: `Nós estamos sempre precisando de pessoas boas protegendo esses oceanos.<br><br>Escolha um barco e boas viagens, marujo.`,
     options:
     [
       {
         buttonText: `Velkor D.E.B.`,
         optionText: `Move-se em todos os sentidos e bem rápido.<br><br>Não se moverá sem combustível, então é melhor coletar energia das algas.<br><br>Calmarias não te pararão.`,
-        image: `images/velkor.png?${Date.now()}`,
+        image: `images/velkor.png`,
         funktion: () =>
         {
           console.log(`velkor`)
@@ -25,7 +25,7 @@ project.stories.portuguese.intro[0] = () =>
       {
         buttonText: `Slatlan`,
         optionText: `Move-se agilmente em qualquer sentido, mas com metade da velocidade se "contra-vento" ou sem gás, obviamente fica parado sem os dois.<br><br>Calmarias serão uma perturbação.`,
-        image: `images/slatan.png?${Date.now()}`,
+        image: `images/slatan.png`,
         funktion: () =>
         {
           console.log(`slatan`)
@@ -35,7 +35,7 @@ project.stories.portuguese.intro[0] = () =>
       {
         buttonText: `Bu Mei`,
         optionText: `"Contra-vento" deixa-o dependente de correntes para girar.<br><br>Não usa gás e vai bem rápido com o vento.<br><br>Calmarias são mortais.`,
-        image: `images/bumei.png?${Date.now()}`,
+        image: `images/bumei.png`,
         funktion: () =>
         {
           console.log(`bumei`)
